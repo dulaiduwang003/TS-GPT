71b486d881bc32acf94ffdccf96bc9d074560677
dulaiduwang003 <87460202+dulaiduwang003@users.noreply.github.com>
2023-03-06 02:12:01 +0800

    Update request.js

--- utils/request.js
+++ utils/request.js
@@ -1,27 +1,46 @@
 import env from '../utils/env';
 
 function service(options = {}) {
-    options.url = `${env.baseUrl}${options.url}`;
-    options.timeout = 500000
-    options.header = {
-        'content-type': 'application/json',
-        "Authorization": "Bearer " + `${env.key}`
-    }
-    return new Promise((resolved, rejected) => {
-        options.success = (res) => {
-            if (res.statusCode !== 200){
-                rejected("OpenAi接口高负载,请稍后再试")
-            }else{
-                resolved(res)
-            }
+	options.url = `${env.baseUrl}${options.url}`;
+	options.timeout = 500000;
+	options.header = {
+		'content-type': 'application/json',
+		"Authorization": "Bearer " + `${env.key}`
+	};
 
-        };
-        options.fail = () => {
-            console.log("执行了A")
-            rejected("OpenAi接口高负载,请稍后再试")
-        };
-        uni.request(options);
-    });
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: `${env.proxy}`,
+			success: function(res) {
+				if (res.statusCode !== 200 || !res.data.success) {
+					reject("获取代理失败");
+					return;
+				}
+				// 使用代理设置
+				const proxy = res.data.data[0];
+				options.proxy = `https://${proxy.ip}:${proxy.port}`;
+				// 发送 HTTP 请求
+				uni.request({
+					...options,
+					success: function(res) {
+							console.log(res);
+						if (res.statusCode !== 200) {
+							reject("OpenAi接口高负载,请稍后再试");
+						} else {
+							resolve(res);
+						}
+					},
+					fail: function(err) {
+						console.log(err);
+						reject("OpenAi接口高负载,请稍后再试");
+					}
+				});
+			},
+			fail: function(err) {
+				reject("获取代理失败");
+			}
+		});
+	});
 }
 
 export default service;
