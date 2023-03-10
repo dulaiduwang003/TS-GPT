2bbef066a2f187f036fd94ed8691f20944129b3f
dulaiduwang003 <87460202+dulaiduwang003@users.noreply.github.com>
2023-03-10 19:54:48 +0800

    Update env.js

--- utils/env.js
+++ utils/env.js
@@ -3,8 +3,8 @@
 export default {
 	//微信APPID
 	appid: 'APPID',
-	//后端域名 可改或不改 数据格式可参考官网示例
-	baseUrl: 'https://www.anchwngb.cn:8000/',
+	//后端域名 数据格式可参考官网示例
+	baseUrl: 'https://XXXX:端口号/',
 	//公共Key
 	key:""
 	
